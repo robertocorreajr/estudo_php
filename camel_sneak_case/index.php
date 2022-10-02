f8f4e4c243a118a6520f4b07319a5e9aeb11c790
Roberto Corrêa <roberto@MacBook-Pro-de-Roberto.local>
2022-10-01 23:58:31 -0300

    Adicionado alguns exercícios que fiz.

---
+++ camel_sneak_case/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// ucfirst -> Coloca apenas a primeira letra em maiusculo.
+// ucwords -> Coloca Todas As Palavras Com A Primeira Letra Em Maiusculo.
+
+$str = "coloca apenas a primeira letra em maiusculo.";
+echo ucfirst($str);
+echo "\n";
+$str = "coloca todas as primeiras letras da palavra em letra maiuscula.";
+echo ucwords($str);
+
+?>
---
+++ lower_upper/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$str = "está string está em caixa baixa";
+$str2 = "ESTÁ OUTRA STRING ESTÁ EM CAIXA ALTA";
+
+echo strtoupper($str);
+echo "\n";
+echo strtolower($str2);
+
+
+?>

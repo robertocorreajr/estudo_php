469a6f5111736bc8655698e9ae6d545f660bc28d
Roberto Corrêa <roberto@MacBook-Pro-de-Roberto.local>
2022-10-18 20:50:05 -0300

    novos ajustes

---
+++ alura/php-oo/Conta.php
@@ -0,0 +1,72 @@
+<?php
+
+class Conta
+{
+    private string $cpfTitular;
+    private string $nomeTitular;
+    private float $saldo;
+
+    public function __construct(string $cpfTitular, string $nomeTitular)
+    {
+        echo "Criando nova conta" . PHP_EOL;
+        $this->cpfTitular = $cpfTitular;
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+    }
+
+    public function sacar(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
+
+    public function defineCpfTitular(string $cpf): void
+    {
+        $this->cpfTitular = $cpf;
+    }
+
+    public function defineNomeTitular(string $nome): void
+    {
+        $this->nomeTitular = $nome;
+    }
+
+    public function recuperarSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function recuperarCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function recuperarNome(): string
+    {
+        return $this->nomeTitular;
+    }
+}
---
+++ alura/php-oo/banco.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'Conta.php';
+
+$primeiraConta = new Conta('123.456.789-10', 'Jhon Doe');
+$primeiraConta->depositar(500);
+$primeiraConta->sacar(300);
+// $primeiraConta->defineCpfTitular('123.456.789-10');
+// $primeiraConta->defineNomeTitular('Jhon Doe');
+
+echo $primeiraConta->recuperarNome() . PHP_EOL;
+echo $primeiraConta->recuperarCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperarSaldo() . PHP_EOL;
+
+echo "\n";
+$segundaConta = new Conta('987.654.321-10', 'Spider Man');
+echo $segundaConta->recuperarNome() . PHP_EOL;
+echo $segundaConta->recuperarCpfTitular() . PHP_EOL;
+echo $segundaConta->recuperarSaldo() . PHP_EOL;
